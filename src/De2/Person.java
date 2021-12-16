19661dd1fb76f372328473b84f31e692733ca883
Đỗ Công Sơn <sonanhson99@gmail.com>
2021-12-16 21:32:30 +0700

    add de2

---
+++ src/De2/Person.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package De2;
+
+/**
+ *
+ * @author sonan
+ */
+public class Person {
+    private String hoTen, ngaySinh, diaChi, gioiTinh;
+
+    public Person() {
+    }
+
+    public Person(String hoTen, String ngaySinh, String diaChi, String gioiTinh) {
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.diaChi = diaChi;
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public void hienThi() {
+        System.out.println("Ho ten: " + this.hoTen);
+        System.out.println("Ngay sinh: " + this.ngaySinh);
+        System.out.println("Dia chi: " + this.diaChi);
+        System.out.println("Gioi tinh: " + this.gioiTinh);
+    }
+}
